b778ffbaa9517523c53b6fce11da88420abf25f9
myles-block <myles.block03@gmail.com>
2022-07-20 15:39:02 -0700

    Added TableView & test values to feed

--- Orion/Orion/View Controllers/HomeFeedViewController.m
+++ Orion/Orion/View Controllers/HomeFeedViewController.m
@@ -6,16 +6,27 @@
 //
 
 #import "HomeFeedViewController.h"
+#import "HomeFeedCell.h"
 
-@interface HomeFeedViewController ()
+@interface HomeFeedViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *feedTableView;
+
 
 @end
 
 @implementation HomeFeedViewController
+NSArray *data;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view.
+    data = @[@"New York, NY", @"Los Angeles, CA", @"Chicago, IL", @"Houston, TX",
+                 @"Philadelphia, PA", @"Phoenix, AZ", @"San Diego, CA", @"San Antonio, TX",
+                 @"Dallas, TX", @"Detroit, MI", @"San Jose, CA", @"Indianapolis, IN",
+                 @"Jacksonville, FL", @"San Francisco, CA", @"Columbus, OH", @"Austin, TX",
+                 @"Memphis, TN", @"Baltimore, MD", @"Charlotte, ND", @"Fort Worth, TX"];
+        self.feedTableView.dataSource = self;
 }
 
 /*
@@ -28,4 +39,14 @@
 }
 */
 
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    HomeFeedCell *cell = [tableView dequeueReusableCellWithIdentifier:@"HomeFeedCell" forIndexPath:indexPath];
+        NSArray *cityState = [data[indexPath.row] componentsSeparatedByString:@", "];
+        cell.productName.text = cityState.firstObject;
+        return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return data.count;
+}
 @end
