58b822058f6c2581ec22ad149ecdacc2f5127da5
myles-block <myles.block03@gmail.com>
2022-07-27 13:07:34 -0700

    initialized Details View for Product

---
+++ Orion/Orion/View Controllers/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  Orion
+//
+//  Created by Myles Block on 7/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Orion/Orion/View Controllers/DetailsViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailsViewController.m
+//  Orion
+//
+//  Created by Myles Block on 7/27/22.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *cancelButton;
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
